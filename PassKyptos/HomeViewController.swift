caeb93dad5954da5d9caae4255b97b718db4b4ac
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-07 23:13:08 -0300

    feat: add UILabel with title

--- PassKyptos/HomeViewController.swift
+++ PassKyptos/HomeViewController.swift
@@ -13,6 +13,13 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .background
+        
+        let label = UILabel()
+        label.text = "PassKyptos"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        
+        view.addSubview(label)
     }
 
 
