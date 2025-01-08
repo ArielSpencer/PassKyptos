b7ba103231719d7685b5e78ee148728a25a90a6b
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-07 23:44:50 -0300

    feat: add constraints to position page title

--- PassKyptos/HomeViewController.swift
+++ PassKyptos/HomeViewController.swift
@@ -15,11 +15,17 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .background
         
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "PassKyptos"
         label.textColor = .white
         label.font = .systemFont(ofSize: 24, weight: .bold)
         
         view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24)
+        ])
     }
 
 
