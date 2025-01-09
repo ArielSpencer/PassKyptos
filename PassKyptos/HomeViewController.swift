8bb18799ecbdec24fdd2f0e1adfb9e7a363a8326
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-08 22:52:53 -0300

    refactor: organize title setup into reusable methods

--- PassKyptos/HomeViewController.swift
+++ PassKyptos/HomeViewController.swift
@@ -8,20 +8,29 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .background
-        
+    
+    private lazy var label: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "PassKyptos"
         label.textColor = .white
         label.font = .systemFont(ofSize: 24, weight: .bold)
-        
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .background
+        addSubViews()
+        setupConstraints()
+    }
+    
+    private func addSubViews() {
         view.addSubview(label)
-        
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24)
